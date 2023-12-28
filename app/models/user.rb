6f73d1072c1d60d8701cd72ce104ef1decc80cf6
Angie Rojas <nataliart4@gmail.com>
2023-12-28 09:41:40 -0500

    Create Message model, table and association with users and display messages in view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, length: {minumum: 3, maximum: 15}
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minumum: 3, maximum: 15}
+
+  has_many :messages
   has_secure_password
 end
